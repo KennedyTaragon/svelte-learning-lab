571a907b7c1f5a305e99a43c2c27867916216f1a
KennedyTaragon <kennedykoech048@gmail.com>
2025-11-02 15:38:48 +0300

    Added Nested Component

---
+++ getting-started/src/Components/Nested.svelte
@@ -0,0 +1,18 @@
+<script>
+    let name = 'Vasco';
+</script>
+
+<main>
+    <h1> {name.toUpperCase()}</h1>
+	<p>My Smart System</p>
+    <p>TEXT FROM THE NESTED COMPONENT</p>
+</main>
+
+
+<style>
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
