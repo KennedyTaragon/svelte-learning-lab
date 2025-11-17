92e16a4e2812af8b3ea537dcce9716f97bfc5639
Vascokenn <kennedykoech048@gmail.com>
2025-11-17 10:58:44 +0300

    Keyed each blocks

---
+++ logic/src/lib/Components/ColorPicker.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  // List of values you want to loop over
+  let colors = ['red', 'green', 'blue', 'purple', 'orange'];
+
+  let selected = $state('');
+</script>
+
+<div class="color-picker">
+  {#each colors as color, i}
+    <button
+      style="background: {color}"
+      aria-label={color}
+      aria-current={selected === color}
+      on:click={() => (selected = color)}
+    >
+      {i + 1}
+    </button>
+  {/each}
+</div>
+
+<p>Selected color: {selected || 'none'}</p>
---
+++ logic/src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    import "../app.css";
+	import ColorPicker from "$lib/Components/ColorPicker.svelte";
+	let count = $state(0);
+
+	function increment() {
+		count += 1;
+	}
+</script>
+
+<div class="main-container">
+<button onclick={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+
+{#if count > 10}
+	<p>{count} is greater than 10</p>
+{:else if count < 5}
+	<p>{count} is less than 5 </p>
+{:else}
+	<p>{count} is between 0 and 10</p>
+{/if}
+
+
+<div class="main-container">
+  <ColorPicker />
+</div>
+</div>
